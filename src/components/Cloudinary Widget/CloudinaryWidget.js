c397163411ef530e942f0c7fd1abe9b2c4f14dbc
Bryan's Mac <bryanpgomes@gmail.com>
2020-08-04 16:02:19 -0400

    Started implementing cloudinary widget

---
+++ src/components/Cloudinary Widget/CloudinaryWidget.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
+
+export default function CloudinaryWidget() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
