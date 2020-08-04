1bee214d164b6ec1558a731f26ef994e32ca0e32
Bryan's Mac <bryanpgomes@gmail.com>
2020-08-04 18:27:00 -0400

    Implmented cloudinary

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.scss';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import NavBar from './NavBar';
-
+import CloudinaryWidget from './Cloudinary Widget/CloudinaryWidget'
 export default function App() {
   return (
+    <>
     <div className="App">
       <CssBaseline />
       <NavBar />
     </div>
+    <div>
+      <CloudinaryWidget />
+    </div>
+    </>
   )
 };
--- src/components/Cloudinary Widget/CloudinaryWidget.js
+++ src/components/Cloudinary Widget/CloudinaryWidget.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
+import React, {useState} from 'react';
 
 export default function CloudinaryWidget() {
+  const [image, setImage] = useState('')
+  const [loading, setLoading] = useState(false)
+  
+  const uploadImage = async e => {
+    const files = e.target.files
+    const data = new FormData()
+
+    data.append('file', files[0])
+    data.append('upload_preset', 'flvjwbbo')
+    setLoading(true)
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/bryanpgomes/image/upload',
+      {
+        method: 'POST',
+        body: data
+      }
+    )
+    const file = await res.json()
+
+    setImage(file.secure_url)
+    setLoading(false)
+  }
+  console.log(image);
   return (
     <div>
-      
+      <h1>Upload Image</h1>
+      <input type="file"
+        name="file"
+        placeholder="Upload an image"
+        onChange={uploadImage}
+      />
+      {loading ? (
+        <h3>Loading...</h3>
+        ): (
+        <img src={image} style={{width: '300px'}}/>
+        )
+      }
     </div>
   )
 }
