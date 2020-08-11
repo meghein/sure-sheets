c374fe6883cbde6ba1119af9ea96c896d30fd894
the1andonlycj <cjhayestutor@gmail.com>
2020-08-10 21:14:09 -0400

    Styled, the bot needs resizing.

--- src/components/Chatbot/BotButton.js
+++ src/components/Chatbot/BotButton.js
@@ -6,7 +6,7 @@ function BotButton(props) {
 
   return (
     <div className="BotButton" onClick={props.toggle}>
-      <img src="/images/NERv05EMPTYSmall.png" alt="It appears to be a very nervous robot, but you can ask him questions if you need to."></img>
+      <img src="/images/NERv05HalfSizeColor.png" alt="It appears to be a very nervous robot, but you can ask him questions if you need to."></img>
     </div>
   );
 }
